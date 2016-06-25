8a11e756aa1a4a5026346074db88bf0ea46ed631
anjinhui <86125622@qq.com>
2016-06-25 18:49:52 +0800

    修改sql并增加字段

--- upgrade/20160621/tbl_users_users_schedule_upgrade.sql
+++ upgrade/20160621/tbl_users_users_schedule_upgrade.sql
@@ -1,2 +1,2 @@
-ALTER TABLE users_schedule add LastRunTime int(11) DEFAULT 0; # 最后执行时间
-ALTER TABLE users_schedule add day tinyint(2) DEFAULT 0;    #间隔执行的天数
+ALTER TABLE users_schedule add LastRunTime int(11) DEFAULT 0 COMMENT "最后执行时间";
+ALTER TABLE users_schedule add day tinyint(2) DEFAULT 0 COMMENT "间隔执行的天数";
