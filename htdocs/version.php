d87f5290ea304220e15b8b238f5e3f5ef2d2b560
sxf <1293812389@qq.com>
2016-07-12 11:31:40 +0800

    修改版本号发V4.1.3.20160712

--- htdocs/version.php
+++ htdocs/version.php
@@ -1,9 +1,9 @@
 <?php
-//V4.1.3.20160711
+//V4.1.3.20160712
 define('IN_UPDATE', true);
 $version = array(
 	'version' => '4.1.3',
-	'release' => '20160711',
+	'release' => '20160712',
 	'product' => 2,
 	'type' => 'simple'
 );
