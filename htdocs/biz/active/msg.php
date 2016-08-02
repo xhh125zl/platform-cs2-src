66f097fc8a419ececb02a640b7e52e86fe986e3f
jinhui <86125622@qq.com>
2016-08-02 09:55:19 +0800

    修改商家后台商家申请活动后消息提示还会显示相应的活动

--- htdocs/biz/active/msg.php
+++ htdocs/biz/active/msg.php
@@ -6,7 +6,7 @@ $action = isset($_GET['action'])?$_GET['action']:"";
 if(IS_AJAX && $action)
 {
     $time = time();
-    $sql = "SELECT * FROM active AS a LEFT JOIN active_type AS t ON a.Type_ID = t.Type_ID WHERE a.Users_ID='{$UsersID}' AND a.starttime<{$time} AND a.stoptime>{$time} AND a.Status=1 ORDER BY a.starttime ASC,a.Active_ID DESC";
+    $sql = "SELECT * FROM active AS a LEFT JOIN active_type AS t ON a.Type_ID = t.Type_ID WHERE a.Users_ID='{$UsersID}' AND a.starttime<{$time} AND a.stoptime>{$time} AND a.Status=1 AND Active_ID NOT IN (SELECT Active_ID FROM biz_active WHERE Users_ID='{$UsersID}' AND Biz_ID={$BizID}) ORDER BY a.starttime ASC,a.Active_ID DESC";
     $result = $DB->query($sql);
     $list = $DB->toArray($result);
     $msglist = [];
