e3e6f9dbf475a25da849441fdfe6cf910ee9b964
jinhui <86125622@qq.com>
2016-08-17 08:41:19 +0800

    修改了活动编辑和添加时js提示不准确的问题，修改了拼团底部菜单显示问题

--- htdocs/api/pintuan/bottom.php
+++ htdocs/api/pintuan/bottom.php
@@ -1,7 +1,15 @@
 <div class="kb"></div>
 <div class="clear"></div>
 <div class="cotrs">
-	<a href="<?=isset($_SESSION["Index_URI"])?$_SESSION["Index_URI"]:'' ?>" <?php echo stripos(basename($_SERVER['REQUEST_URI']),'act') !==false ?'class="thisclass"':'' ?>><img src="/static/api/pintuan/images/002-1.png" width="25px" height="25px" /><br />首页</a>
-	<a href="<?php echo "/api/$UsersID/pintuan/seach/0/"; ?>" <?php echo basename($_SERVER['REQUEST_URI']) == "0"?'class="thisclass"':'' ?>><img src="/static/api/pintuan/images/002-2.png" width="22px" height="22px" style="margin-top: 3px;" /><br />搜索</a>
-	<a href="<?php echo "/api/$UsersID/pintuan/user/"; ?>" <?php echo basename($_SERVER['REQUEST_URI']) == "user"?'class="thisclass"':'' ?>><img src="/static/api/pintuan/images/002-3.png" width="22px" height="22px" style="margin-top: 3px;" /><br />我的</a>
+	<a href="<?=isset($_SESSION["Index_URI"])?$_SESSION["Index_URI"]:'' ?>"
+		<?php echo stripos(basename($_SERVER['REQUEST_URI']),'act') !== false ? 'class="thisclass"':'' ?>><img
+		src="/static/api/pintuan/images/002-1.png" width="25px" height="25px" /><br />首页</a>
+	<a href="<?php echo "/api/$UsersID/pintuan/seach/0/"; ?>"
+		<?php echo basename($_SERVER['REQUEST_URI']) == "0"?'class="thisclass"':'' ?>><img
+		src="/static/api/pintuan/images/002-2.png" width="22px" height="22px"
+		style="margin-top: 3px;" /><br />搜索</a> <a
+		href="<?php echo "/api/$UsersID/pintuan/user/"; ?>"
+		<?php echo basename($_SERVER['REQUEST_URI']) == "user"?'class="thisclass"':'' ?>><img
+		src="/static/api/pintuan/images/002-3.png" width="22px" height="22px"
+		style="margin-top: 3px;" /><br />我的</a>
 </div>
