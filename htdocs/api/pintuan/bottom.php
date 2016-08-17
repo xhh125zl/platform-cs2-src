ed4cd8cf60f8ed23f5a7108a00469253fd92b4a1
jinhui <86125622@qq.com>
2016-08-17 16:02:10 +0800

    修复了拼团底部菜单色块错位显示，修复了高访问下订单无法提交的Bug，修复了抽奖后退款SQL语句的简化

--- htdocs/api/pintuan/bottom.php
+++ htdocs/api/pintuan/bottom.php
@@ -1,15 +1,28 @@
+<?php 
+    $request_uri = $_SERVER['REQUEST_URI'];
+    $first_parm = basename(dirname($request_uri));
+    $end_parm = basename($request_uri);
+    $isDefault = 'index';
+    if($first_parm == 'pintuan' && stripos($end_parm,'act') !==false) { //首页
+        $isDefault = 'index';
+    }else if (in_array($first_parm, [ 'seach','sousuo' ])) {
+        $isDefault = 'search';
+    }else{
+        $isDefault = 'user';
+    }
+?>
 <div class="kb"></div>
 <div class="clear"></div>
 <div class="cotrs">
 	<a href="<?=isset($_SESSION["Index_URI"])?$_SESSION["Index_URI"]:'' ?>"
-		<?php echo stripos(basename($_SERVER['REQUEST_URI']),'act') !== false ? 'class="thisclass"':'' ?>><img
+		<?=$isDefault == 'index' ? 'class="thisclass"':'' ?>><img
 		src="/static/api/pintuan/images/002-1.png" width="25px" height="25px" /><br />首页</a>
 	<a href="<?php echo "/api/$UsersID/pintuan/seach/0/"; ?>"
-		<?php echo basename($_SERVER['REQUEST_URI']) == "0"?'class="thisclass"':'' ?>><img
+		<?=$isDefault == 'search' ? 'class="thisclass"':'' ?>><img
 		src="/static/api/pintuan/images/002-2.png" width="22px" height="22px"
 		style="margin-top: 3px;" /><br />搜索</a> <a
 		href="<?php echo "/api/$UsersID/pintuan/user/"; ?>"
-		<?php echo basename($_SERVER['REQUEST_URI']) == "user"?'class="thisclass"':'' ?>><img
+		<?=$isDefault == 'user' ? 'class="thisclass"':'' ?>><img
 		src="/static/api/pintuan/images/002-3.png" width="22px" height="22px"
 		style="margin-top: 3px;" /><br />我的</a>
 </div>
