0695de19979127f85032ba8909e91699aae36624
xvjinkui@163.com <xvjinkui@163.com>
2016-09-21 08:58:33 +0800

    增加了一些样式,删除了无用的代码

--- htdocs/user/lib/upload.php
+++ htdocs/user/lib/upload.php
@@ -1,7 +1,7 @@
 <?php
 
 if ($_POST['act'] == 'uploadFile') {
-    
+
 } elseif ($_POST['act'] == 'delImg') {
     $imagepath = htmlspecialchars(trim($_POST['image_path']));
     $index = (int)$_POST['index'];
