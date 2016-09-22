1d71d7bf0208ff09a1467ebb2b8f0ac9cea19a61
sxf <1293812389@qq.com>
2016-09-22 14:44:02 +0800

    店铺配置

--- htdocs/user/setting.php
+++ htdocs/user/setting.php
@@ -1,3 +1,15 @@
+<?php
+if (!defined('USER_PATH')) exit();
+
+require_once CMS_ROOT . '/include/api/shopconfig.class.php';
+
+$data = [
+    'Biz_Account' => $BizAccount,
+];
+$result = shopconfig::getConfig($data);
+$config = $result['data'];
+
+?>
 <!doctype html>
 <html>
 <head>
@@ -18,41 +30,48 @@
     <div class="shop_list">
     	<ul>
         	<li>
-            	<a><span class="l">店铺头像<span>店铺头像</span></span><span class="r"><i class="fa  fa-angle-right fa-2x" aria-hidden="true"></i></span></a>
+            	<a><span class="l">店铺头像<span><img src='<?php echo $config['ShopLogo'];?>' width=30 height=30 /></span></span><span class="r"><i class="fa  fa-angle-right fa-2x" aria-hidden="true"></i></span></a>
             </li>
             <li>
-            	<a><span class="l">店铺名称<span>你是我的小苹果</span></span><span class="r"><i class="fa  fa-angle-right fa-2x" aria-hidden="true"></i></span></a>
+            	<a><span class="l">店铺名称<span><?php echo $config['ShopName'];?></span></span><span class="r"><i class="fa  fa-angle-right fa-2x" aria-hidden="true"></i></span></a>
             </li>
             <li>
-            	<a><span class="l">微信号<span>000000</span></span><span class="r"><i class="fa  fa-angle-right fa-2x" aria-hidden="true"></i></span></a>
+            	<a><span class="l">微信号<span><?php echo $config['Users_WechatAccount'];?></span></span><span class="r"><i class="fa  fa-angle-right fa-2x" aria-hidden="true"></i></span></a>
             </li><!--
             <li>
             	<a><span class="l">独立域名<span>zzzzzzzzzzzzz</span></span><span class="r"><i class="fa  fa-angle-right fa-2x" aria-hidden="true"></i></span></a>
             </li>-->
             <li>
-            	<a><span class="l">店铺分享<span>店铺分享</span></span><span class="r"><i class="fa  fa-angle-right fa-2x" aria-hidden="true"></i></span></a>
+            	<a><span class="l">店铺分享<span><?php
+                    if (trim($config['ShareIntro']) != '') {
+                        echo sub_str($config['ShareIntro'], 16);
+                    }
+                 ?></span></span><span class="r"><i class="fa  fa-angle-right fa-2x" aria-hidden="true"></i></span></a>
             </li>
             <li>
             	<a><span class="l">店铺二维码<span>店铺二维码</span></span><span class="r"><i class="fa  fa-angle-right fa-2x" aria-hidden="true"></i></span></a>
             </li>
         	<li>
-            	<a><span class="l">店铺公告<span>店铺公告</span></span><span class="r"><i class="fa  fa-angle-right fa-2x" aria-hidden="true"></i></span></a>
+            	<a><span class="l">店铺公告<span><?php
+                    if (trim($config['ShopAnnounce']) != '') {
+                        echo sub_str($config['ShopAnnounce'], 16);
+                    }
+                 ?></span></span><span class="r"><i class="fa  fa-angle-right fa-2x" aria-hidden="true"></i></span></a>
+            </li>
+
+            <li>
+            	<a><span class="l">退货地址<span><?php echo $config['RecieveAddress'];?></span></span><span class="r"><i class="fa  fa-angle-right fa-2x" aria-hidden="true"></i></span></a>
             </li>
             <li>
-            	<a><span class="l">联系电话<span>联系电话</span></span><span class="r"><i class="fa  fa-angle-right fa-2x" aria-hidden="true"></i></span></a>
+            	<a><span class="l">退货电话<span><?php echo $config['RecieveMobile'];?></span></span><span class="r"><i class="fa  fa-angle-right fa-2x" aria-hidden="true"></i></span></a>
             </li>
             <li>
-            	<a><span class="l">退货地址<span>退货地址</span></span><span class="r"><i class="fa  fa-angle-right fa-2x" aria-hidden="true"></i></span></a>
+            	<a><span class="l">自动收货时间<span><?php if ($config['Confirm_Time'] > 0) { echo $config['Confirm_Time'] / 86400;} else {echo 0;}?>天</span></span><span class="r"><i class="fa  fa-angle-right fa-2x" aria-hidden="true"></i></span></a>
             </li>
             <li>
-            	<a><span class="l">自动收货时间<span>自动收货时间</span></span><span class="r"><i class="fa  fa-angle-right fa-2x" aria-hidden="true"></i></span></a>
+            	<span class="l">分销商是否可以代销其他产品</span><span class="r"><input class="toggle-switch" type="checkbox"<?php if ($config['allowDistributeB2c'] == 1) echo ' checked=""'?>></span>
             </li>
-            <li>
-            	<span class="l">分销商是否可以代销其他产品</span><span class="r"><input class="toggle-switch" type="checkbox" checked=""></span>
-            </li>
-            <li>
-            	<span class="l">产品放入产品库，千万V客帮你代销</span><span class="r"><input class="toggle-switch" type="checkbox" checked=""></span>
-            </li>
+
         </ul>
     </div>
 </div>
