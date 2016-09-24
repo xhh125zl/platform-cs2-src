6460b1bf410fe09d0553c95378045fc35933dc70
sxf <1293812389@qq.com>
2016-09-24 17:10:50 +0800

    登录页面添加手机号登录placeholder

--- htdocs/user/login.php
+++ htdocs/user/login.php
@@ -90,12 +90,12 @@ if (isset($_GET['do']) && $_GET['do'] == 'logout') {
 	<p class="login_title">登录</p>
     <div class="login_box">
     <form id="form1" name="form1" method="post">
-    	<input type="text" name="Account" id="Account" value="" maxlength="11" class="login_x" placeholder="登录用户名" notnull="">
+    	<input type="text" name="Account" id="Account" value="" maxlength="11" class="login_x" placeholder="登录名/手机号" notnull="">
         <input type="password" name="Password" id="Password" value="" maxlength="16" class="login_x1" placeholder="登录密码" notnull="">
         <input name="提交" type="button" class="login_sub" value="立即登录">
             <div class="login_t">
-               <span class="l"><a href="#">忘记密码？</a></span>
-               <span class="r"><a href="#">立即注册</a></span>
+               <span class="l"><a href="forget.php">忘记密码？</a></span>
+               <span class="r"><a href="reg.php">立即注册</a></span>
         </div>
     </form>
     </div>
