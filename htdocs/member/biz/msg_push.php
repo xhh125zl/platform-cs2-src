b184e1c60f7a5a64761a5a153bed1b2cf48a601e
xvjinkui <xvjinkui@163.com>
2016-10-14 15:42:51 +0800

    加入了推送消息管理分页功能

--- htdocs/member/biz/msg_push.php
+++ htdocs/member/biz/msg_push.php
@@ -4,7 +4,7 @@ if(empty($_SESSION["Users_Account"]))
     header("location:/member/login.php");
 }
 $lists = [];
-$DB->getPage("msg_push","*","where 1",2);
+$DB->getPage("msg_push","*","where 1",20);
 
 while($r=$DB->fetch_assoc()){
     $lists[] = $r;
@@ -14,3 +14,6 @@ foreach($lists as $k=>$msg){
 <?= $msg['id']?> 内容为:<?=$msg['content']?>,发送时间为<?=date('Y-m-d H:i:s', $msg['create_time'])?><br/>
 <?
 }
+
+$DB->showPage();
+?>
