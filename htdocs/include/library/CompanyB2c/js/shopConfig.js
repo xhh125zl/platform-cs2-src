e6ecf44a9723fb9b08ea9265ae3d9380d4b1edcb
2591218372@qq.com <2591218372@qq.com>
2016-10-17 17:22:13 +0800

    商家发布自营产品收费金额改成开关

--- htdocs/include/library/CompanyB2c/js/shopConfig.js
+++ htdocs/include/library/CompanyB2c/js/shopConfig.js
@@ -2,13 +2,13 @@
  * Created by Administrator on 2016/9/10.
  */
 $(function() {
-    $("#Users_PayCharge").click(function() {
+	/*$("#Users_PayCharge").click(function() {
         $(this).unbind("click");
         $(this).html('<input type="text" class="input" name="Users_PayCharge" maxlength="30" placeholder="请填写收费金额,鼠标点击其他地方会自动保存" onkeyup="this.value=this.value.replace(/[^\\d.]/g,\'\')"  />');
         $("input[name=Users_PayCharge]").focus();
-    });
-    $("input[name=Users_PayCharge]").live("blur",function(){
-        var Users_PayCharge = $(this).val();
+    });*/
+    $("input[name=Users_PayCharge]").live("change",function(){
+        var Users_PayCharge = $(this).val();//alert(Users_PayCharge);return false;
         if (Users_PayCharge.length == 0) {
             //这里可以执行一些长度为空的时候的判断和逻辑
         }else{
@@ -22,22 +22,22 @@ $(function() {
                         if (data.status == 1) {
                             layer.msg("更新成功", {icon:6, time:500}, function(){
                                 layer.close(indexloader);
-                                $("#Users_PayCharge").html('<p style="padding-left:30px;">&yen;'+data.Users_PayCharge+'元</p>');
+                                /*$("#Users_PayCharge").html('<p style="padding-left:30px;">&yen;'+data.Users_PayCharge+'元</p>');
                                 $("#Users_PayCharge").bind("click", function() {
                                     $(this).unbind("click");
                                     $(this).html('<input type="text" class="input" name="Users_PayCharge"	 maxlength="30" placeholder="请填写收费金额,鼠标点击其他地方会自动保存" onkeyup="this.value=this.value.replace(/[^\\d.]/g,\'\')"  />');
                                     $("input[name=Users_PayCharge]").focus();
-                                });
+                                });*/
                             });
                         }else{
                             layer.msg("更新失败,请联系管理员", {icon:5, time:2000}, function(){
                                 layer.close(indexloader);
-                                $("#Users_PayCharge").html('<p style="padding-left:30px;">&yen;'+data.Users_PayCharge+'元</p>');
+                                /*$("#Users_PayCharge").html('<p style="padding-left:30px;">&yen;'+data.Users_PayCharge+'元</p>');
                                 $("#Users_PayCharge").bind("click", function() {
                                     $(this).unbind("click");
                                     $(this).html('<input type="text" class="input" name="Users_PayCharge"	 maxlength="30" placeholder="请填写收费金额,鼠标点击其他地方会自动保存" onkeyup="this.value=this.value.replace(/[^\\d.]/g,\'\')"  />');
                                     $("input[name=Users_PayCharge]").focus();
-                                });
+                                });*/
                             });
                         }
                     });
