0f9f1ef9f580e35245f221e4bbcf9cdfc8cbabc6
2591218372@qq.com <2591218372@qq.com>
2016-10-17 17:39:23 +0800

    商家入驻文章表

--- upgrade/20161008/biz_article.sql
+++ upgrade/20161008/biz_article.sql
@@ -1,23 +1,3 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : localhost
-Source Server Version : 50540
-Source Host           : localhost:3306
-Source Database       : 103hbqw999
-
-Target Server Type    : MYSQL
-Target Server Version : 50540
-File Encoding         : 65001
-
-Date: 2016-09-07 16:37:07
-*/
-
-SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for biz_article
--- ----------------------------
 DROP TABLE IF EXISTS `biz_article`;
 CREATE TABLE `biz_article` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
