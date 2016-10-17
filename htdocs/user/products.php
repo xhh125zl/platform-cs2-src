683d2115844d7882d80711434d611ba2b20c18b6
sxf <1293812389@qq.com>
2016-10-17 14:58:46 +0800

    注释掉佣金显示

--- htdocs/user/products.php
+++ htdocs/user/products.php
@@ -286,7 +286,7 @@ if (count($infolist) > 0)        	 {
                     <span class="main l">
                         <p><?php echo $product['Products_Name'];?> <?php echo $product['Products_ID'];?></p>
                         <span class="l" style="font-size:16px; line-height:25px; color:#333">￥<?php echo $product['Products_PriceX'];?></span>
-                        <!--<span class="r" style="line-height:25px;">佣金<sss style="color:#ff5000; font-size:16px;">￥<?php /*echo ($product['Products_PriceY'] - $product['Products_PriceX'])*/?><sss/></span>-->
+                        <!--<span class="r" style="line-height:25px;">佣金<sss style="color:#ff5000; font-size:16px;">￥<?php /*echo ($product['Products_PriceY'] - $product['Products_PriceX'])*/?></sss></span>-->
                         <div class="clear"></div>
                         <span class="l">已售<?php echo $product['Products_Sales'];?></span>
                         <span class="r">库存:<?php echo $product['Products_Count'];?></span>
@@ -340,7 +340,7 @@ if (count($infolist) > 0)        	 {
             <span class="main l">
                 <p>{{product.Products_Name}}【{{product.Products_ID}}】</p>
                 <span class="l" style="font-size:16px; line-height:25px; color:#333">￥{{product.Products_PriceX}}</span>
-                <span class="r" style="line-height:25px;">佣金<sss style="color:#ff5000; font-size:16px;">￥{{product.Products_PriceY - product.Products_PriceX}}<sss/></span>
+                <!--<span class="r" style="line-height:25px;">佣金<sss style="color:#ff5000; font-size:16px;">￥{{product.Products_PriceY - product.Products_PriceX}}</sss></span>-->
                 <div class="clear"></div>
                 <span class="l">已售{{product.Products_Sales}}</span>
                 <span class="r">库存:{{product.Products_Count}}</span>
