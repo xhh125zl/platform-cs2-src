9534e89963fd5c84f6e9c74986fc1baa8b6a145b
xhh <1042340690@qq.com>
2016-11-02 09:29:21 +0800

    修改信息自动加载报错问题

--- htdocs/user/lib/message.php
+++ htdocs/user/lib/message.php
@@ -1,38 +1,40 @@
 <?php
-require_once CMS_ROOT . "/user/config.inc.php";
+require_once "config.inc.php";
 require_once CMS_ROOT . '/include/api/message.class.php';
 require_once CMS_ROOT . '/include/helper/page.class.php';
 
-//系统消息未读条数
-//获取商家注册时间，以确认显示信息
-$biz_info = $DB->GetRs('biz', 'Biz_CreateTime', 'where `Biz_ID` = '.$BizID);
-$DB->Get("announce","announce.*,announce_record.Record_ID","left join `announce_record` on announce.Announce_ID = announce_record.Announce_ID and announce_record.Biz_ID = ".$BizID." where announce.Announce_Status = 1 and Announce_CreateTime > ".$biz_info['Biz_CreateTime']." order by announce_record.Record_ID,announce.Announce_CreateTime desc");
-$unread_system_nums = 0;
-while ($r=$DB->fetch_assoc()) {
-    if (!(isset($r['Record_ID']) && $r['Record_ID'] > 0)) {
-        $unread_system_nums++;
+if (!$_POST) {
+    //系统消息未读条数
+    //获取商家注册时间，以确认显示信息
+    $biz_info = $DB->GetRs('biz', 'Biz_CreateTime', 'where `Biz_ID` = '.$BizID);
+    $DB->Get("announce","announce.*,announce_record.Record_ID","left join `announce_record` on announce.Announce_ID = announce_record.Announce_ID and announce_record.Biz_ID = ".$BizID." where announce.Announce_Status = 1 and Announce_CreateTime > ".$biz_info['Biz_CreateTime']." order by announce_record.Record_ID,announce.Announce_CreateTime desc");
+    $unread_system_nums = 0;
+    while ($r=$DB->fetch_assoc()) {
+        if (!(isset($r['Record_ID']) && $r['Record_ID'] > 0)) {
+            $unread_system_nums++;
+        }
+    }
+    //订单消息未读条数
+    $result = message::getMsgOrder(['Biz_Account' => $BizAccount]);
+    $unread_order_nums = 0;
+    if ($result['errorCode'] == 0) {
+        $unread_order_nums = $result['data']['unReadCount'];
+    }
+    //分销消息未读条数
+    $result = message::getMsgDistribute(['Biz_Account' => $BizAccount]);
+    $unread_distribute_nums = 0;
+    if ($result['errorCode'] == 0) {
+        $unread_distribute_nums = $result['data']['unReadCount'];
+    }
+    //提现消息未读条数
+    $result = message::getMsgWithdraw(['Biz_Account' => $BizAccount]);
+    $unread_withdraw_nums = 0;
+    if ($result['errorCode'] == 0) {
+        $unread_withdraw_nums = $result['data']['unReadCount'];
     }
 }
-//订单消息未读条数
-$result = message::getMsgOrder(['Biz_Account' => $BizAccount]);
-$unread_order_nums = 0;
-if ($result['errorCode'] == 0) {
-    $unread_order_nums = $result['data']['unReadCount'];
-}
-//分销消息未读条数
-$result = message::getMsgDistribute(['Biz_Account' => $BizAccount]);
-$unread_distribute_nums = 0;
-if ($result['errorCode'] == 0) {
-    $unread_distribute_nums = $result['data']['unReadCount'];
-}
-//提现消息未读条数
-$result = message::getMsgWithdraw(['Biz_Account' => $BizAccount]);
-$unread_withdraw_nums = 0;
-if ($result['errorCode'] == 0) {
-    $unread_withdraw_nums = $result['data']['unReadCount'];
-}
 
-if ($_POST) {
+if ($_POST && !isset($_POST['ajax'])) {
     $do = $_GET['act'];
     if ($do == 'msg_system') {      //记录商家读取信息
         $Announce_ID = $_POST['Announce_ID'];
